d1e78e83d39b63f8efe64058ad68927036bf96e6
qwery1237 <jinsu4768@gmail.com>
2022-09-08 11:55:17 -0700

    Update LabTask2.java

--- src/LabTask2.java
+++ src/LabTask2.java
@@ -1,3 +1,17 @@
 public class LabTask2 {
-    
+    static int[] duplicate(int[] arr) {
+        int [] newArr = arr;
+        for (int i = 0; i < newArr.length; i++) {
+            if (newArr[i] == 0) {
+                for (int j = i + 1; j < newArr.length - 1; j++) {
+                    newArr[j + 1] = newArr[j];
+                }
+                newArr[i+1] = 0;
+            }
+        }
+        return newArr;
+    }
+    public static void main(String[] args) {
+
+    }
 }
