bcfba38f04298bfe17c79dbda96f6507ae999e02
Jinsoo <jinsu4768@gmail.com>
2022-09-15 23:34:08 -0700

    modify task2

--- Week2/src/task2.java
+++ Week2/src/task2.java
@@ -1,32 +1,27 @@
 import java.util.Arrays;
 
 public class task2 {
-    static String[] remove(int[] arr, int num) {
+    static int[] remove(int[] arr, int num) {
 
-        String[] strArr = new String[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            strArr[i] = String.valueOf(arr[i]);
-        }
+        int[] newArr = arr;
+        int size = arr.length;
 
         for (int i = arr.length -1; i >= 0; i--) {
-            if (arr[i] == num){
-                for (int j = i; j< arr.length; j++){
-                    if (j == arr.length -1) strArr[j] = "_";
-                    else strArr[j] = strArr[j+1];
-                }
+            if (arr[i] == num) {
+                size--;
+                for (int j = i; j< arr.length -1 ; j++) newArr[j] = newArr[j+1];
             }
 
         }
-        return strArr;
+        return Arrays.copyOf(newArr,size);
     }
     public static void main(String[] args) {
-        int[] testArr = {1,2,3,4};
-        int[] testArr2 = {1,2,5,3,5,3,2,2,4,2};
-        int[] testArr3 = {1,3,2,3,3,4};
+        int[] testArr = remove(new int[]{ 1, 2, 3, 4 },2);
+        int[] testArr2 = remove(new int[]{1,2,5,3,5,3,2,2,4,2},2);
+        int[] testArr3 = remove(new int[]{1,3,2,3,3,4},3);
 
-        System.out.println(Arrays.toString(remove(testArr,2)));
-        System.out.println(Arrays.toString(remove(testArr2,2)));
-        System.out.println(Arrays.toString(remove(testArr3,3)));
+        System.out.println(testArr.length + ", testArr = " + Arrays.toString(testArr));
+        System.out.println(testArr2.length + ", testArr2 = " + Arrays.toString(testArr2));
+        System.out.println(testArr3.length + ", testArr3 = " + Arrays.toString(testArr3));
     }
 }
